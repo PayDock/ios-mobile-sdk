5c588ff0450f7f66227fbd34740b909ed9ed3e3f
Domagoj Grizelj <domagoj.grizelj@paydock.com>
2023-07-05 12:57:06 +0200

    Setup min versions, build platforms, test proj, build settings

---
+++ MobileSDK/MobileSDK/MobileSDK.h
@@ -0,0 +1,18 @@
+//
+//  MobileSDK.h
+//  MobileSDK
+//
+//  Created by Domagoj Grizelj on 05.07.2023..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobileSDK.
+FOUNDATION_EXPORT double MobileSDKVersionNumber;
+
+//! Project version string for MobileSDK.
+FOUNDATION_EXPORT const unsigned char MobileSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileSDK/PublicHeader.h>
+
+
---
+++ MobileSDK/MobileSDKTests/MobileSDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  MobileSDKTests.swift
+//  MobileSDKTests
+//
+//  Created by Domagoj Grizelj on 05.07.2023..
+//
+
+import XCTest
+@testable import MobileSDK
+
+class MobileSDKTests: XCTestCase {
+
+    func testAlwaysSucceeds() {
+        assert(true)
+    }
+
+}
