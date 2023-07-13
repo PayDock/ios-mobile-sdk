15deb08004653b6e99e0c333630975246c77793d
Domagoj Grizelj <domagoj.grizelj@paydock.com>
2023-07-13 20:15:36 +0200

    Implemented SSL pinning via public key

---
+++ MobileSDK/Sources/MobileSDK/Network/Base/SSLPinningManager.swift
@@ -0,0 +1,72 @@
+//
+//  SSLPinningManager.swift
+//  MobileSDK
+//
+//  Created by Domagoj Grizelj on 13.07.2023..
+//
+
+import Foundation
+import CommonCrypto
+
+final class SSLPinningManager: NSObject {
+
+    // MARK: - Properties
+
+    let publicKeyHash = "NGKfDItRieVy86A22OPQZDKvZrhSBgA9JpgrGv8veAk="
+    let rsa2048Asn1Header:[UInt8] = [0x30, 0x82, 0x01, 0x22, 0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86,0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00, 0x03, 0x82, 0x01, 0x0f, 0x00];
+
+    // MARK: - Helpers
+
+    private func sha256(data : Data) -> String {
+        var keyWithHeader = Data(rsa2048Asn1Header)
+        keyWithHeader.append(data)
+
+        var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
+        keyWithHeader.withUnsafeBytes { buffer in
+            _ = CC_SHA256(buffer.baseAddress!, CC_LONG(buffer.count), &hash)
+        }
+
+        return Data(hash).base64EncodedString()
+    }
+    
+}
+
+// MARK: - URLSessionDelegate
+
+extension SSLPinningManager: URLSessionDelegate {
+
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+
+        guard let serverTrust = challenge.protectionSpace.serverTrust else {
+            completionHandler(.cancelAuthenticationChallenge, nil);
+            return
+        }
+
+        if let serverCertificate = SecTrustGetCertificateAtIndex(serverTrust, 1) {
+            // For Leaf 0, For Intermediate 1, For Root 2(In this tutorial we have used Leaf Certificate)
+            // Server public key
+            let serverPublicKey = SecCertificateCopyKey(serverCertificate)
+
+            // Server public key Data
+            let serverPublicKeyData = SecKeyCopyExternalRepresentation(serverPublicKey!, nil )!
+            let data:Data = serverPublicKeyData as Data
+
+            // Server Hash key
+            let serverHashKey = sha256(data: data)
+            // Local Hash Key
+            let publickKeyLocal = publicKeyHash
+
+            if (serverHashKey == publickKeyLocal) {
+                print("SSL Pinnign with Public key successfully completed.")
+                completionHandler(.useCredential, URLCredential(trust:serverTrust))
+                return
+            }
+
+            else {
+                completionHandler(.cancelAuthenticationChallenge,nil)
+                print("SSL Pinning with Public key has failed.")
+            }
+        }
+    }
+
+}
