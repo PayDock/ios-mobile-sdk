99781cd74322ac6f928efa8d6ab36ca64930a748
Domagoj Grizelj <domagoj.grizelj@paydock.com>
2023-07-25 18:43:51 +0200

    Refactor setting screen implementation

---
+++ ExampleApp/ExampleApp/CustomViews/PickerView.swift
@@ -0,0 +1,54 @@
+//
+//  PickerView.swift
+//  ExampleApp
+//
+//  Created by Domagoj Grizelj on 25.07.2023..
+//
+
+import SwiftUI
+
+struct PickerView: View {
+
+    @State var entries: [String]
+    @State var selected: String
+    @State var placeholder: String
+
+    var onSelection: (String) -> Void
+
+    var body: some View {
+        VStack {
+            Menu {
+                ForEach(entries, id: \.self) { client in
+                    Button(client) {
+                        self.selected = client
+                        onSelection(selected)
+                    }
+                }
+            } label: {
+                VStack {
+                    ZStack {
+                        Rectangle()
+                            .frame(height: 40)
+                            .foregroundColor(Color.white)
+                            .padding(.horizontal, 16)
+
+                        HStack {
+                            Text(selected.isEmpty ? placeholder : selected)
+                                .foregroundColor(.black)
+                                .padding(.leading, 32)
+                            Spacer()
+                            Image("angle-down")
+                                .padding(.trailing, 32)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct PickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerView(entries: ["Font 1, Font 2"], selected: "", placeholder: "Select something", onSelection: {_ in })
+    }
+}
---
+++ ExampleApp/ExampleApp/Screens/Settings/SettingsVM.swift
@@ -0,0 +1,42 @@
+//
+//  SettingsVM.swift
+//  ExampleApp
+//
+//  Created by Domagoj Grizelj on 21.07.2023..
+//
+
+import Foundation
+import SwiftUI
+
+class SettingsVM: ObservableObject {
+
+    // MARK: - Dependencies
+
+    private let projectEnvironment: ProjectEnvironment
+
+    // MARK: - Properties
+
+    @Published var selectedEnvironment = ""
+    var languages = ["English"]
+    var selectedLanguage = "English"
+    var secretKey = ""
+
+    // MARK: - Initialization
+
+    init(projectEnvironment: ProjectEnvironment = .shared) {
+        self.projectEnvironment = projectEnvironment
+    }
+
+    func getProjectEnvironmentNames() -> [String] {
+        return ProjectEnvironment.Environment.allCases.map { $0.rawValue }
+    }
+
+    func getSelectedEnvironmentEndpoint() -> String {
+        return projectEnvironment.getEnvironmentEndpoint(for: selectedEnvironment)
+    }
+
+    func copySecretKey() {
+        UIPasteboard.general.string = secretKey
+    }
+
+}
