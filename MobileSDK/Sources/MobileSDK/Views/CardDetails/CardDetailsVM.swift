591a2d0d430a29ec5b93652ea482cf6f3df6cc4b
Domagoj Grizelj <domagoj.grizelj@paydock.com>
2023-08-04 00:37:18 +0200

    Code polish

--- MobileSDK/Sources/MobileSDK/Views/CardDetails/CardDetailsVM.swift
+++ MobileSDK/Sources/MobileSDK/Views/CardDetails/CardDetailsVM.swift
@@ -9,8 +9,22 @@ import Foundation
 
 class CardDetailsVM: ObservableObject {
 
+    // MARK: - Properties
+
     @Published var hint1 = "Hint 1"
     @Published var hint2 = "Hint 2"
+
+    @Published var text1Valid = true {
+      didSet {
+        hint1 = text1Valid ? "Hint 1" : "Error 1"
+      }
+    }
+
+    @Published var text2Valid = true {
+      didSet {
+        hint2 = text2Valid ? "Hint 2" : "Error 2"
+      }
+    }
 
     let placeholder1 = "Placeholder 1"
     let placeholder2 = "Placeholder 2"
@@ -18,17 +32,31 @@ class CardDetailsVM: ObservableObject {
     var text1: String = ""
     var text2: String = ""
 
-    @Published var text1Valid = true {
-      didSet {
-        hint1 = text1Valid ? "Hint 1" : "Error 1"
-      }
+    var editingTextField1 = false {
+        didSet {
+            guard editingTextField1 != oldValue else { return }
+
+            if editingTextField1 {
+                editingTextField2 = false
+            } else {
+                validateText1()
+            }
+        }
     }
-    
-    @Published var text2Valid = true {
-      didSet {
-        hint2 = text2Valid ? "Hint 2" : "Error 2"
-      }
+
+    var editingTextField2 = false {
+        didSet {
+            guard editingTextField2 != oldValue else { return }
+
+            if editingTextField2 {
+                editingTextField1 = false
+            } else {
+                validateText2()
+            }
+        }
     }
+
+    // MARK: - Methods
 
     func validateText1() {
       text1Valid.toggle() // Test validation.
--- MobileSDK/Sources/MobileSDK/Views/CardDetails/CardDetailsView.swift
+++ MobileSDK/Sources/MobileSDK/Views/CardDetails/CardDetailsView.swift
@@ -15,30 +15,6 @@ struct CardDetailsView: View {
     @FocusState private var textFieldInFocus: CardDetailsFocusable?
     @State var isPresented = false
 
-    @State private var editingTextField1 = false {
-        didSet {
-            guard editingTextField1 != oldValue else { return }
-
-            if editingTextField1 {
-                editingTextField2 = false
-            } else {
-                viewModel.validateText1()
-            }
-        }
-    }
-
-    @State private var editingTextField2 = false {
-        didSet {
-            guard editingTextField2 != oldValue else { return }
-
-            if editingTextField2 {
-                editingTextField1 = false
-            } else {
-                viewModel.validateText2()
-            }
-        }
-    }
-
     // MARK: - View protocol properties
 
     var body: some View {
@@ -47,33 +23,29 @@ struct CardDetailsView: View {
                 $viewModel.text1,
                 placeholder: viewModel.placeholder1,
                 hint: $viewModel.hint1,
-                editing: $editingTextField1,
+                editing: $viewModel.editingTextField1,
                 valid: $viewModel.text1Valid)
             .focused($textFieldInFocus, equals: .text1)
             .padding(.horizontal, 16)
             .onTapGesture {
                 self.textFieldInFocus = .text1
-                self.editingTextField1 = true
+                self.viewModel.editingTextField1 = true
             }
 
             OutlineTextField(
                 $viewModel.text2,
                 placeholder: viewModel.placeholder2,
                 hint: $viewModel.hint2,
-                editing: $editingTextField2,
+                editing: $viewModel.editingTextField2,
                 valid: $viewModel.text2Valid)
             .focused($textFieldInFocus, equals: .text2)
             .padding(.horizontal, 16)
             .onTapGesture {
                 self.textFieldInFocus = .text2
-                self.editingTextField2 = true
+                self.viewModel.editingTextField2 = true
             }
             
             Spacer()
-        }
-        .onTapGesture {
-            editingTextField1 = false
-            editingTextField2 = false
         }
     }
 
