4b5f8bcbedcce5931884da6d6cbf24bd2f156a80
Domagoj Grizelj <domagoj.grizelj@paydock.com>
2023-08-03 23:08:26 +0200

    Improve OutlineTextField switching in card details layout

--- MobileSDK/Sources/MobileSDK/Views/CardDetails/CardDetailsVM.swift
+++ MobileSDK/Sources/MobileSDK/Views/CardDetails/CardDetailsVM.swift
@@ -9,33 +9,59 @@ import Foundation
 
 class CardDetailsVM: ObservableObject {
 
+    @Published var hint1 = "Hint 1"
+    @Published var hint2 = "Hint 2"
+
+    let placeholder1 = "Placeholder 1"
+    let placeholder2 = "Placeholder 2"
+
     var text1: String = ""
     var text2: String = ""
 
-    var editingTextField1 = false {
-        didSet {
-            guard editingTextField1 != oldValue else {
-                return
-            }
-            if editingTextField1 {
-                editingTextField2 = false
-            }
-        }
+    @Published var text1Valid = true {
+      didSet {
+        hint1 = text1Valid ? "Hint 1" : "Error 1"
+      }
+    }
+    @Published var text2Valid = true {
+      didSet {
+        hint2 = text2Valid ? "Hint 2" : "Error 2"
+      }
+    }
+//
+//    var editingTextField1 = false {
+//        didSet {
+//            guard editingTextField1 != oldValue else {
+//                return
+//            }
+//            if editingTextField1 {
+//                editingTextField2 = false
+//            }
+//        }
+//    }
+//
+//    var editingTextField2 = false {
+//        didSet {
+//            guard editingTextField2 != oldValue else {
+//                return
+//            }
+//            if editingTextField2 {
+//                editingTextField1 = false
+//            }
+//        }
+//    }
+
+//    func cancelEditing() {
+//        editingTextField1 = false
+//        editingTextField2 = false
+//    }
+
+    func validateText1() {
+      text1Valid.toggle() // Demonstrative dummy validation.
     }
 
-    var editingTextField2 = false {
-        didSet {
-            guard editingTextField2 != oldValue else {
-                return
-            }
-            if editingTextField2 {
-                editingTextField1 = false
-            }
-        }
+    func validateText2() {
+      text2Valid.toggle() // Demonstrative dummy validation.
     }
-
-    func cancelEditing() {
-        editingTextField1 = false
-        editingTextField2 = false
-    }
+    
 }
--- MobileSDK/Sources/MobileSDK/Views/CardDetails/CardDetailsView.swift
+++ MobileSDK/Sources/MobileSDK/Views/CardDetails/CardDetailsView.swift
@@ -13,23 +13,58 @@ struct CardDetailsView: View {
     @StateObject private var viewModel = CardDetailsVM()
     @FocusState private var text1InFocus: Bool
 
+
+    @State
+    private var editingTextField1 = false {
+        didSet {
+            guard editingTextField1 != oldValue else {
+                return
+            }
+            if editingTextField1 {
+                editingTextField2 = false
+            } else {
+                viewModel.validateText1()
+            }
+        }
+    }
+    @State
+    private var editingTextField2 = false {
+        didSet {
+            guard editingTextField2 != oldValue else {
+                return
+            }
+            if editingTextField2 {
+                editingTextField1 = false
+            } else {
+                viewModel.validateText2()
+            }
+        }
+    }
+
     var body: some View {
         VStack {
-            OutlineTextField($viewModel.text1, editing: $viewModel.editingTextField1)
-                .onTapGesture { viewModel.editingTextField1 = true }
-                .focused($text1InFocus)
-                .onAppear {
-                  DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
-                    self.text1InFocus = true
-                  }
-                }
+            OutlineTextField(
+                $viewModel.text1,
+                placeholder: viewModel.placeholder1,
+                hint: $viewModel.hint1,
+                editing: $editingTextField1,
+                valid: $viewModel.text1Valid)
+            .padding()
+            .onTapGesture { editingTextField1 = true }
 
-            OutlineTextField($viewModel.text2, editing: $viewModel.editingTextField2)
-                .onTapGesture { viewModel.editingTextField2 = true }
+            OutlineTextField(
+                $viewModel.text2,
+                placeholder: viewModel.placeholder2,
+                hint: $viewModel.hint2,
+                editing: $editingTextField2,
+                valid: $viewModel.text2Valid)
+            .padding()
+            .onTapGesture { editingTextField2 = true }
             Spacer()
         }
+        .contentShape(Rectangle())
         .onTapGesture {
-            viewModel.cancelEditing()
+//            viewModel.cancelEditing()
         }
     }
 }
