ec9bb956266ad5797839c9fb52f37ef34caf88ea
Domagoj Grizelj <domagoj.grizelj@gmail.com>
2024-01-08 15:09:10 +0100

    Set all environment tokens and endpoints to sandbox

--- ExampleApp/ExampleApp/ProjectEnvironment.swift
+++ ExampleApp/ExampleApp/ProjectEnvironment.swift
@@ -35,25 +35,31 @@ struct ProjectEnvironment {
 
     func getEnvironmentEndpoint() -> String {
         switch environment {
-        case .production: return "api.paydock.com"
+            // TODO: - Change endpoint for prod and staging. All environments currently use sandbox endpopint
+        case .production: return "api-sandbox.paydock.com"
+//        case .production: return "api.paydock.com"
         case .sandbox: return "api-sandbox.paydock.com"
-        case .staging: return "apista.paydock.com"
+            // TODO: - Change endpoint for prod and staging. All environments currently use sandbox endpopint
+        case .staging: return "api-sandbox.paydock.com"
+//        case .staging: return "apista.paydock.com"
         }
     }
 
     func getPublicKey() -> String {
         switch environment {
-        case .production: return ""
+            // TODO: - Change tokens for prod and staging. All environments currently use Sanbox tokens
+        case .production: return "b3e5eafcbf585cd298fc66fdc6b449505e64bf8d"
         case .sandbox: return "b3e5eafcbf585cd298fc66fdc6b449505e64bf8d"
-        case .staging: return ""
+        case .staging: return "b3e5eafcbf585cd298fc66fdc6b449505e64bf8d"
         }
     }
 
     func getSecretKey() -> String {
         switch environment {
-        case .production: return ""
+            // TODO: - Change tokens for prod and staging. All environments currently use Sanbox tokens
+        case .production: return "ee7c98ebab4780bcd2d8f562e1499d2ce806f7c1"
         case .sandbox: return "ee7c98ebab4780bcd2d8f562e1499d2ce806f7c1"
-        case .staging: return ""
+        case .staging: return "ee7c98ebab4780bcd2d8f562e1499d2ce806f7c1"
         }
     }
 }
--- MobileSDK/Sources/MobileSDK/Constants.swift
+++ MobileSDK/Sources/MobileSDK/Constants.swift
@@ -15,9 +15,10 @@ struct Constants {
         }
         
         switch environment {
-        case .production: return "api.paydock.com"
-        case .sandbox: return "api-sandbox.paydock.com"
-        case .staging: return "apista.paydock.com"
+            // TODO: - Update the endpoints to proper values once CI output is configured
+//        case .production: return "api.paydock.com"
+        case .sandbox, .production, .staging: return "api-sandbox.paydock.com"
+//        case .staging: return "apista.paydock.com"
         }
     }
 
@@ -27,9 +28,10 @@ struct Constants {
         }
 
         switch environment {
-        case .production: return ""
-        case .sandbox: return "b3e5eafcbf585cd298fc66fdc6b449505e64bf8d"
-        case .staging: return ""
+            // TODO: - Update token to proper values after CI output is configured
+//        case .production: return ""
+        case .sandbox, .production, .staging: return "b3e5eafcbf585cd298fc66fdc6b449505e64bf8d"
+//        case .staging: return ""
         }
     }
 }
