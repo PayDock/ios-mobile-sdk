8b228827b31b881e9a7b1b930cbe068d9204e9fc
Domagoj Grizelj <domagoj.grizelj@gmail.com>
2024-01-26 14:20:01 +0100

    Modify Package.swift structure

--- Package.swift
+++ Package.swift
@@ -23,13 +23,15 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MobileSDK",
-            dependencies: []),
+            dependencies: [],
+            path: "MobileSDK/Sources"),
         .testTarget(
             name: "MobileSDKTests",
             dependencies: ["MobileSDK"],
+            path: "MobileSDK/Tests",
             resources: [
-                .copy("Resources/JSON/card_tokenisation_error_response.json"),
-                .copy("Resources/JSON/card_tokenisation_success_response.json")]
+                .copy("MobileSDKTests/Resources/JSON/card_tokenisation_error_response.json"),
+                .copy("MobileSDKTests/Resources/JSON/card_tokenisation_success_response.json")]
         ),
     ]
 )
