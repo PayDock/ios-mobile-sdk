b00c5ab3e3443ced249db3508e8d4937760e1aa6
Domagoj Grizelj <domagoj.grizelj@gmail.com>
2024-05-24 12:00:06 +0200

    Finalise privacy policy implementation

---
+++ MobileSDK/Sources/MobileSDK/Models/CardDetails/CardResult.swift
@@ -0,0 +1,14 @@
+//
+//  CardResult.swift
+//  MobileSDK
+//
+//  Created by Domagoj Grizelj on 23.05.2024..
+//  Copyright © 2024 Paydock Ltd.
+//
+
+import Foundation
+
+public struct CardResult {
+    public let token: String
+    public let saveCard: Bool?
+}
