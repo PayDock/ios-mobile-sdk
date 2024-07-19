d4017601c30c3223f27867f946f1266bce53b801
Domagoj Grizelj <domagoj.grizelj@gmail.com>
2024-07-19 14:21:42 +0200

    Update package on main project

--- Package.swift
+++ Package.swift
@@ -18,13 +18,16 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/afterpay/sdk-ios", exact: "5.4.0"),
+        .package(url: "https://gitlab.com/paydock/bounded-contexts/mobile/mobile-lib-networking-ios", branch: "develop")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MobileSDK",
-            dependencies: [.product(name: "Afterpay", package: "sdk-ios")],
+            dependencies: [
+                .product(name: "Afterpay", package: "sdk-ios"),
+                .product(name: "NetworkingLib", package: "mobile-lib-networking-ios")]
             path: "MobileSDK/Sources"),
         .testTarget(
             name: "MobileSDKTests",
