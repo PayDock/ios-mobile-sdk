8aa8134ac1de9dd3e0bbcc92588ecf49724be2e1
Domagoj Grizelj <domagoj.grizelj@gmail.com>
2024-10-16 14:26:00 +0200

    Entry list

---
+++ ExampleApp/ExampleApp/Screens/Widgets/PayPalVault/PayPalVaultWidgetView.swift
@@ -0,0 +1,28 @@
+//
+//  PayPalVaultWidgetView.swift
+//  ExampleApp
+//
+//  Created by Domagoj Grizelj on 14.10.2024..
+//  Copyright © 2024 Paydock Ltd. All rights reserved.
+//
+
+import SwiftUI
+import MobileSDK
+
+struct PayPalVaultWidgetView: View {
+
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                
+            }
+            .background(Color(hex: "#EAE0D7"))
+        }
+    }
+}
+
+struct PayPalVaultWidgetView_Previews: PreviewProvider {
+    static var previews: some View {
+        PayPalVaultWidgetView()
+    }
+}
